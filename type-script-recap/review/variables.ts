ff6e1e1360bad84f5781cdf52323e0ca264a29ea
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-03-04 11:29:57 -0500

    initial file

---
+++ type-script-recap/review/variables.ts
@@ -0,0 +1,20 @@
+// string, numbers, booleans, null, undefined
+// 1. String: A sequence of characters
+const myString: string = "Hello World";
+console.log(myString);
+
+// 2. Numbers: Represent a quantity or amount
+const myNumber: number = 42;
+console.log(myNumber);
+
+// 3. Boolean: Represents true or false values
+const isDone: boolean = true;
+console.log(isDone);
+
+// 4. Null: Used to indicate that something is empty/does not exist
+let x = null;
+console.log(x);
+
+// 5. Undefined: Used to indicate that a variable has been declared but not assigned a value yet
+let y;
+console.log(y);
