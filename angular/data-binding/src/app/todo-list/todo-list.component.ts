16e8a9e2bd5455b1d9e094aef8137bab14db56bb
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-03-11 12:46:47 -0400

    ngModel two-way data binding

---
+++ angular/data-binding/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Todo } from '../models/todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css'],
+})
+export class TodoListComponent {
+  // properties
+  newTodoTitle: string = '';
+  newTodoDate: Date = new Date();
+
+  todo: Todo[] = [];
+
+  addTodo() {
+    // validate fields
+    if (this.newTodoTitle.trim().length && this.newTodoDate) {
+      this.newTodoTitle = '';
+      let newTodo: Todo = {
+        id: Date.now(),
+        title: this.newTodoTitle,
+        date: this.newTodoDate,
+      };
+      this.todo.push(newTodo);
+      this.newTodoTitle = '';
+      this.newTodoDate = new Date();
+    }
+  }
+}
