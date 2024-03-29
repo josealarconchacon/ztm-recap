d946398e20c7cbca6a8b61ae85dc9efdbf9c4f7a
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-03-28 20:21:52 -0400

    list view add it

--- angular/service-form-routing/src/app/app.module.ts
+++ angular/service-form-routing/src/app/app.module.ts
@@ -1,18 +1,22 @@
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { ReservationModule } from './reservation/reservation.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HomeModule,
+    ReservationModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
