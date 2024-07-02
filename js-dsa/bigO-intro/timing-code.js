fe08285ce26d8e4bc3e6754958b4ed09cc691181
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-07-02 16:12:41 -0400

    Measure execution time

--- js-dsa/bigO-intro/timing-code.js
+++ js-dsa/bigO-intro/timing-code.js
@@ -13,13 +13,14 @@ function sumOfNumbers(n) {
 }
 // console.log(sumOfNumbers(6));
 
-// function sumOfNumbers01(n) {
-//   return (n * (n + 1)) / 2;
-// }
+function sumOfNumbers01(n) {
+  // faster time
+  return (n * (n + 1)) / 2;
+}
 
 // Measure execution time
 const startTime = performance.now();
-sumOfNumbers(100000000);
+sumOfNumbers01(100000000);
 const endTime = performance.now();
 
 // Calculate and log the elapsed time
