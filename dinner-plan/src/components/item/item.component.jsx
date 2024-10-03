bdcabe39468f6870a6eb6dfa54885ea749f4b631
josealarconchacon <jose.alarcon.chacon@gmail.com>
2024-10-03 16:57:01 -0400

    updating items in list

--- dinner-plan/src/components/item/item.component.jsx
+++ dinner-plan/src/components/item/item.component.jsx
@@ -1,23 +1,3 @@
-// import React from "react";
-// import "./item.style.scss";
-
-// function Item({ item, onDeleteItem, onToggleItems }) {
-//   return (
-//     <li className="item-card">
-//       <input
-//         type="checkbox"
-//         value={item.packed}
-//         onChange={() => onToggleItems(item.id)}
-//       />
-//       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-//         {item.quantity} {item.description}
-//       </span>
-//       <button onClick={() => onDeleteItem(item.id)}>❌</button>
-//     </li>
-//   );
-// }
-
-// export default Item;
 import React from "react";
 import "./item.style.scss";
 
